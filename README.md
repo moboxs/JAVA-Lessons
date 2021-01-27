e193ae963b554869a8e40e3eca6c763020f9666e
moboxs <liuhonghao@jd.com>
2021-01-27 11:00:16 +0800

    week01

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/JAVA-000/JAVA-000/issues
---
+++ week01/src/main/java/com/github/moboxs/week01/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.moboxs.week01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+    }
+}
